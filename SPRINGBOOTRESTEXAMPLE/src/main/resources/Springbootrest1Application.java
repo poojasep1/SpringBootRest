f26668cb4ab57f82d3c94047f638d1740fd3c5e4
pooja <pooja.lebaku9@gmail.com>
2021-05-04 22:25:32 +0530

    intial commit

---
+++ SPRINGBOOTRESTEXAMPLE/src/main/resources/Springbootrest1Application.java
@@ -0,0 +1,11 @@
+package com.niit.SPRINGBOOTRESTEXAMPLE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootrest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootrest1Application.class, args);
+	}
---
+++ SPRINGBOOTRESTEXAMPLE/src/main/resources/Springbootrest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.SPRINGBOOTRESTEXAMPLE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootrest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
